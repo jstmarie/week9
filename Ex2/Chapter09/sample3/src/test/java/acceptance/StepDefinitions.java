48b8e04cec8165ca742ceb72d86d2422a1b1a72d
Justin St. Marie <justin_stmarie@student.uml.edu>
2023-03-26 14:47:30 -0400

    more

---
+++ Ex2/Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -0,0 +1,58 @@
+package acceptance;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertEquals;
+
+/** Steps definitions for calculator.feature */
+public class StepDefinitions {
+    private String server = System.getProperty("calculator.url");
+
+    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate2 = new RestTemplate();
+
+    private String a;
+    private String b;
+    private String result;
+    private String dividend;
+    private String divisor;
+    private String quotient;
+
+    @Given("^I have two numbers: (.*) and (.*)$")
+    public void i_have_two_numbers(String a, String b) throws Throwable {
+        this.a = a;
+        this.b = b;
+    }
+
+    @When("^the calculator sums them$")
+    public void the_calculator_sums_them() throws Throwable {
+        String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
+        result = restTemplate.getForObject(url, String.class);
+    }
+
+    @Then("^I receive (.*) as a result$")
+    public void i_receive_as_a_result(String expectedResult) throws Throwable {
+        assertEquals(expectedResult, result);
+    }
+
+    //divide
+    @Given("^I have a dividend and divisor: (.*) and (.*)$")
+    public void i_have_a_dividend_and_divisor(String divisor, String dividend) throws Throwable {
+        this.divisor = divisor;
+        this.dividend = dividend;
+    }
+
+    @When("^the calculator divides them$")
+    public void the_calculator_divides_them() throws Throwable {
+        String url = String.format("%s/div?a=%s&b=%s", server, divisor, dividend);
+        quotient = restTemplate2.getForObject(url, String.class);
+    }
+
+    @Then("^the quotient is (.*)$")
+    public void the_quotient_is(String expectedResult) throws Throwable {
+        assertEquals(expectedResult, quotient);
+    }
+}
